e521136ed9641cf857448c2646e61bd7cd099589
Will Coleda <coke@cpan.org>
2010-06-22 12:55:56 +0000

    Import of COKE/Task-Parrot-0.01 from CPAN.
    gitpan-cpan-distribution: Task-Parrot
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         COKE/Task-Parrot-0.01.tar.gz
    gitpan-cpan-author:       COKE
    gitpan-cpan-maturity:     released

---
+++ lib/Task/Parrot.pm
@@ -0,0 +1,42 @@
+package Task::Parrot;
+
+#ABSTRACT: Collection of perl modules that help to hack on parrot.
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Task::Parrot - Collection of perl modules that help to hack on parrot.
+
+=head1 VERSION
+
+version 0.01
+
+=head1 TASK CONTENTS
+
+=head2 Modules for testing
+
+=head3 L<TAP::Archive>
+
+=head3 L<TAP::Archive::Harness>
+
+=head2 Modules for tools
+
+=head3 L<Test::Differences>
+
+=head1 AUTHOR
+
+Will "Coke" Coleda <will@coleda.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2010 by Will "Coke" Coleda <will@coleda.com>.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0
+
+=cut
+
